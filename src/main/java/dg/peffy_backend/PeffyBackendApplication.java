0c2f531cfc7de36c5a4b8b2232f1161d3146ace7
DanyK94 <d.giraldi.94@gmail.com>
2026-09-24 10:42:07 +0200

    init peffy rep

---
+++ src/main/java/dg/peffy_backend/PeffyBackendApplication.java
@@ -0,0 +1,13 @@
+package dg.peffy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeffyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeffyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dg/peffy_backend/PeffyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dg.peffy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeffyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
